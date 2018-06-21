875d357916d7edeb33e49c4e10601a3f8417b13e
EricJoin1 <40448503+EricJoin1@users.noreply.github.com>
2018-06-21 13:20:32 +0800

    Update Schedule.java

--- src/main/java/com/migu/schedule/Schedule.java
+++ src/main/java/com/migu/schedule/Schedule.java
@@ -1,43 +1,92 @@
 package com.migu.schedule;
 
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import com.migu.schedule.constants.ReturnCodeKeys;
 import com.migu.schedule.info.TaskInfo;
-
-import java.util.List;
 
 /*
 *类名和方法不能修改
  */
 public class Schedule {
-
-
+	List<TaskInfo> tak = null;
+	Map<Integer,Integer> guaqimap = new HashMap<Integer,Integer>();
     public int init() {
-        // TODO 方法未实现
-        return ReturnCodeKeys.E000;
+    	tak = new ArrayList<TaskInfo>();
+        return ReturnCodeKeys.E001;
     }
 
 
     public int registerNode(int nodeId) {
-        // TODO 方法未实现
-        return ReturnCodeKeys.E000;
+    	if(nodeId < 0)
+    	{
+    		return ReturnCodeKeys.E004;
+    	}
+		for(TaskInfo taskInfo:tak)
+		{
+			if(nodeId == taskInfo.getNodeId())
+			{
+				return ReturnCodeKeys.E005;
+			}
+		}
+		TaskInfo taskInfo = new TaskInfo();
+		taskInfo.setNodeId(nodeId);
+		tak.add(taskInfo);
+        return ReturnCodeKeys.E003;
     }
 
     public int unregisterNode(int nodeId) {
-        // TODO 方法未实现
-        return ReturnCodeKeys.E000;
+    	if(nodeId < 0)
+    	{
+    		return ReturnCodeKeys.E004;
+    	}
+    	for(TaskInfo taskInfo:tak)
+		{
+			if(nodeId == taskInfo.getNodeId())
+			{
+				tak.remove(nodeId);
+				return ReturnCodeKeys.E006;
+			}
+		}
+        return ReturnCodeKeys.E007;
     }
 
 
     public int addTask(int taskId, int consumption) {
-        // TODO 方法未实现
-        return ReturnCodeKeys.E000;
+    	if(taskId < 0)
+    	{
+    		return ReturnCodeKeys.E009;
+    	}
+    	for (Integer key : guaqimap.keySet()) {
+    	      if(taskId == key)
+    	      {
+    	    	  return ReturnCodeKeys.E010;
+    	      }
+    	    }
+    	guaqimap.put(taskId, consumption);
+        return ReturnCodeKeys.E008;
     }
 
 
     public int deleteTask(int taskId) {
-        // TODO 方法未实现
-        return ReturnCodeKeys.E000;
+    	if(taskId < 0)
+    	{
+    		return ReturnCodeKeys.E009;
+    	}
+    	for(TaskInfo taskInfo:tak)
+		{
+			if(taskId == taskInfo.getTaskId())
+			{
+				taskInfo.setTaskId(0);
+				tak.add(taskInfo);
+				return ReturnCodeKeys.E011;
+			}
+		}
+        return ReturnCodeKeys.E012;
     }
 
 
@@ -48,8 +97,37 @@ public class Schedule {
 
 
     public int queryTaskStatus(List<TaskInfo> tasks) {
-        // TODO 方法未实现
-        return ReturnCodeKeys.E000;
+    	if(null == tasks)
+    	{
+    		return ReturnCodeKeys.E016;
+    	}
+    	List<TaskInfo> tasks1 = new ArrayList<TaskInfo>();
+    	for(TaskInfo taskInfo:tak)
+    	{
+    		for(TaskInfo taskInfo1:tasks)
+    		{
+    			if(taskInfo.getTaskId() == taskInfo1.getTaskId())
+    			{
+    				tasks1.add(taskInfo);
+    			}
+    		}
+    	}
+    	for (Integer key : guaqimap.keySet()) {
+    		for(TaskInfo taskInfo1:tasks)
+    		{
+    			if(taskInfo1.getTaskId() == key)
+    			{
+    				taskInfo1.setNodeId(-1);
+    				tasks1.add(taskInfo1);
+    			}
+    		}
+  	    }
+    	tasks.removeAll(tasks);
+    	for(TaskInfo taskInfo1:tasks1)
+    	{
+    		tasks.add(taskInfo1);
+    	}
+        return ReturnCodeKeys.E015;
     }
 
 }
